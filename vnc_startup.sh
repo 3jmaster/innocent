c1bb6f82e3c65204ee189d54f409e9170c905307
Fabian Lim <fabian_lim@ida-gds.com>
2018-07-02 15:17:46 +0800

    add vnc scripts

---
+++ vnc_startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+### every exit != 0 fails the script
+set -e
+
+## change vnc password
+echo -e "\n------------------ change VNC password  ------------------"
+# first entry is control, second is view (if only one is valid for both)
+mkdir -p "$HOME/.vnc"
+PASSWD_PATH="$HOME/.vnc/passwd"
+if [[ $VNC_VIEW_ONLY == "true" ]]; then
+    echo "start VNC server in VIEW ONLY mode!"
+    #create random pw to prevent access
+    echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
+fi
+echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+chmod 600 $PASSWD_PATH
+
+echo -e "\n------------------ start VNC server ------------------------"
+echo "remove old vnc locks to be a reattachable container"
+vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
+    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $STARTUPDIR/vnc_startup.log \
+    || echo "no locks present"
+
+echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
+if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION &> $STARTUPDIR/no_vnc_startup.log
+echo -e "start window manager\n..."
+$HOME/wm_startup.sh &> $STARTUPDIR/wm_startup.log
+
+## log connect options
+echo -e "\n\n------------------ VNC environment started ------------------"
+echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
+echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
